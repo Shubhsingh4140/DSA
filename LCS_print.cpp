f18680a826a20674b490cbeedbdb139620e7aa9c
Shubhsingh4140 <141202844+Shubhsingh4140@users.noreply.github.com>
2025-07-04 13:52:02 +0530

    Create LCS_print.cpp

---
+++ LCS_print.cpp
@@ -0,0 +1,65 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+ int LCS(string X,string Y,int n,int m){
+     string op="";
+        
+        int  dp[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                }
+                
+            }
+        }
+        int maxLen=0;
+        
+          for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(X[i-1]==Y[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    op=op+X[i-1];
+                    int x=dp[i][j];
+                    // maxLen=max(x,maxLen);
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+               
+            }
+        }
+          for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+              cout<<dp[i][j]<<" ";
+                
+            }
+            cout<<endl;
+        }
+        cout<<op<<endl;
+        return dp[n][m];
+
+
+    }
+
+
+    
+   
+
+
+int main() {
+    // Sample input
+   
+    
+    string X="ace";
+    string Y="abbbbcsse";
+    int n=X.length();
+    int m=Y.length();
+
+    int no = LCS( X,Y,n,m);
+
+    cout << "Length: " << no << endl;
+    return 0;
+}
