2f04f48d8661d5b091ed766234cedca88dda5760
Shubhsingh4140 <141202844+Shubhsingh4140@users.noreply.github.com>
2025-07-17 12:18:01 +0530

    Create Kth_smallest_element.cpp

---
+++ Heap/Kth_smallest_element.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <climits>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+vector<int> nums;
+nums.push_back(7);
+nums.push_back(10);
+nums.push_back(4);
+nums.push_back(3);
+nums.push_back(20);
+nums.push_back(15);
+int k=3;
+priority_queue<int> maxHeap;
+for(int i:nums){
+    maxHeap.push(i);
+    if(maxHeap.size()>k){
+        maxHeap.pop();
+    }
+}
+cout<<maxHeap.top();
+
+
+
+    return 0;
+}
